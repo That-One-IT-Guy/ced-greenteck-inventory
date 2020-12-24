386b93921fcf5ee2e6133b0ba368c84983291f22
Jack Frederick <73072330+py1h0n-h4xer@users.noreply.github.com>
2020-12-23 23:47:07 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import time
+
+print("Welcome user! Please wait...")
+print("Thank you, as just a reminder this application only sais where it should go and cant say if it is there, moved, or out of stauk. Sorry for the inconvinice.")
+print("BETA VERSION DETEDCTED REPORT BUGS TO J49FKK@GMAIL.COM")
+print("What can I find for you?")
+time.sleep(10)
